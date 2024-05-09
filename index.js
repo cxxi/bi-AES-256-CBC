89a6e7bf1af6a39e941adc81051860670e020f47
cxxi <l.a_lumet@yahoo.fr>
2024-05-10 01:08:37 +0200

    Demo

---
+++ index.js
@@ -0,0 +1,49 @@
+'use strict'
+
+const crypto = require('crypto')
+
+
+class Demo
+{
+	static encrypt(message)
+	{
+	    try
+	    {
+	    	if (process.versions.openssl <= '1.0.1f') {
+		        throw new Error('OpenSSL Version too old, vulnerability to Heartbleed')
+		    }
+
+		    let iv = crypto.randomBytes(crypto.getCipherInfo('AES-256-CBC').ivLength)
+		    let cipher = crypto.createCipheriv('AES-256-CBC', new Buffer(process.env.PRIVATE_KEY), iv)
+		    let encrypted = cipher.update(message)
+		    encrypted = Buffer.concat([encrypted, cipher.final()])
+
+		    return `${iv.toString('hex')}:${encrypted.toString('hex')}`
+	    } 
+
+	    catch(error)
+	    {
+	    	console.error(error)
+	    }
+	}
+
+	static decrypt(hashedMessage)
+	{
+    	try
+    	{
+			let bin = hashedMessage.toString().split(':')
+		    let iv = new Buffer(bin.shift(), 'hex')
+		    let encryptedText = new Buffer(bin.join(':'), 'hex')
+		    let decipher = crypto.createDecipheriv('AES-256-CBC', new Buffer(process.env.PRIVATE_KEY), iv)
+		    let decrypted = decipher.update(encryptedText)
+
+		    decrypted = Buffer.concat([decrypted, decipher.final()])
+		    return decrypted.toString()
+	    }
+
+	    catch(error)
+	    {
+	        console.error(error)
+	    }
+	}
+}
---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+
+class Demo
+{
+	public static function encrypt(string $message): string
+	{
+		try
+		{
+			if (OPENSSL_VERSION_NUMBER <= 268443727) {
+		        throw new RuntimeException('OpenSSL Version too old, vulnerability to Heartbleed');
+		    }
+
+		  	$iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length('AES-256-CBC'));
+		    
+		    $ciphertext = openssl_encrypt($message, 'AES-256-CBC', $_ENV['PRIVATE_KEY'], OPENSSL_RAW_DATA, $iv);
+
+		    $hashedMessage = implode(':', [ bin2hex($iv), bin2hex($ciphertext) ]);
+
+		    return $hashedMessage;
+		}
+
+		catch(\Exception $exception)
+		{
+			echo $exception->getMessage().PHP_EOL;
+		}
+	}
+
+	public static function decrypt(string $hashedMessage): string
+	{
+		try
+		{
+		    [ $iv, $ciphertext ] = array_map(fn($hex) => hex2bin($hex), explode(":", $hashedMessage));
+		 
+		    $message = openssl_decrypt($ciphertext, 'AES-256-CBC', $_ENV['PRIVATE_KEY'], OPENSSL_RAW_DATA, $iv);
+
+		    return $message;
+		}
+
+		catch(\Exception $exception)
+		{
+			echo $exception->getMessage().PHP_EOL;
+		}
+	}
+}
