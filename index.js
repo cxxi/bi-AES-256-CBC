59af76910a36da316fa1a5e25d85dbc0172bf741
Louis-Antoine LUMET <l.a_lumet@yahoo.fr>
2024-11-06 17:45:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,15 +10,15 @@ class Demo
 	    try
 	    {
 	    	if (process.versions.openssl <= '1.0.1f') {
-		        throw new Error('OpenSSL Version too old, vulnerability to Heartbleed')
-		    }
+		    throw new Error('OpenSSL Version too old, vulnerability to Heartbleed')
+		}
 
-		    let iv = crypto.randomBytes(crypto.getCipherInfo('AES-256-CBC').ivLength)
-		    let cipher = crypto.createCipheriv('AES-256-CBC', new Buffer(process.env.PRIVATE_KEY), iv)
-		    let encrypted = cipher.update(message)
-		    encrypted = Buffer.concat([encrypted, cipher.final()])
+		let iv = crypto.randomBytes(crypto.getCipherInfo('AES-256-CBC').ivLength)
+		let cipher = crypto.createCipheriv('AES-256-CBC', new Buffer(process.env.PRIVATE_KEY), iv)
+		let encrypted = cipher.update(message)
+		encrypted = Buffer.concat([encrypted, cipher.final()])
 
-		    return `${iv.toString('hex')}:${encrypted.toString('hex')}`
+		return `${iv.toString('hex')}:${encrypted.toString('hex')}`
 	    } 
 
 	    catch(error)
@@ -29,16 +29,16 @@ class Demo
 
 	static decrypt(hashedMessage)
 	{
-    	try
-    	{
-			let bin = hashedMessage.toString().split(':')
-		    let iv = new Buffer(bin.shift(), 'hex')
-		    let encryptedText = new Buffer(bin.join(':'), 'hex')
-		    let decipher = crypto.createDecipheriv('AES-256-CBC', new Buffer(process.env.PRIVATE_KEY), iv)
-		    let decrypted = decipher.update(encryptedText)
+    	    try
+    	    {
+		let bin = hashedMessage.toString().split(':')
+		let iv = new Buffer(bin.shift(), 'hex')
+		let encryptedText = new Buffer(bin.join(':'), 'hex')
+		let decipher = crypto.createDecipheriv('AES-256-CBC', new Buffer(process.env.PRIVATE_KEY), iv)
+		let decrypted = decipher.update(encryptedText)
 
-		    decrypted = Buffer.concat([decrypted, decipher.final()])
-		    return decrypted.toString()
+		decrypted = Buffer.concat([decrypted, decipher.final()])
+		return decrypted.toString()
 	    }
 
 	    catch(error)
