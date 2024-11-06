5a202ad3f616c65c4081595869c3332120e4cb59
Louis-Antoine LUMET <l.a_lumet@yahoo.fr>
2024-11-06 17:46:31 +0100

    Update index.php

--- index.php
+++ index.php
@@ -6,17 +6,17 @@ class Demo
 	{
 		try
 		{
-			if (OPENSSL_VERSION_NUMBER <= 268443727) {
-		        throw new RuntimeException('OpenSSL Version too old, vulnerability to Heartbleed');
-		    }
+		    	if (OPENSSL_VERSION_NUMBER <= 268443727) {
+				throw new RuntimeException('OpenSSL Version too old, vulnerability to Heartbleed');
+		    	}
 
-		  	$iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length('AES-256-CBC'));
+			$iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length('AES-256-CBC'));
 		    
-		    $ciphertext = openssl_encrypt($message, 'AES-256-CBC', $_ENV['PRIVATE_KEY'], OPENSSL_RAW_DATA, $iv);
+			$ciphertext = openssl_encrypt($message, 'AES-256-CBC', $_ENV['PRIVATE_KEY'], OPENSSL_RAW_DATA, $iv);
 
-		    $hashedMessage = implode(':', [ bin2hex($iv), bin2hex($ciphertext) ]);
+			$hashedMessage = implode(':', [ bin2hex($iv), bin2hex($ciphertext) ]);
 
-		    return $hashedMessage;
+			return $hashedMessage;
 		}
 
 		catch(\Exception $exception)
@@ -29,11 +29,11 @@ class Demo
 	{
 		try
 		{
-		    [ $iv, $ciphertext ] = array_map(fn($hex) => hex2bin($hex), explode(":", $hashedMessage));
+			[ $iv, $ciphertext ] = array_map(fn($hex) => hex2bin($hex), explode(":", $hashedMessage));
 		 
-		    $message = openssl_decrypt($ciphertext, 'AES-256-CBC', $_ENV['PRIVATE_KEY'], OPENSSL_RAW_DATA, $iv);
+			$message = openssl_decrypt($ciphertext, 'AES-256-CBC', $_ENV['PRIVATE_KEY'], OPENSSL_RAW_DATA, $iv);
 
-		    return $message;
+			return $message;
 		}
 
 		catch(\Exception $exception)
